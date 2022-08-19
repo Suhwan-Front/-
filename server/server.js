fc66a2377fcc6a6f1863a5764432d6b6b776a392
Suhwan0818 <tnghks1997@gmail.com>
2022-08-20 00:45:54 +0900

    ✅ 완료파일

--- server/server.js
+++ server/server.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.get("/", (req, res) => {
-  res.send(`success message`);
+app.get("/api/login", (req, res) => {
+  res.send({ ID: "test", Password: "1234" });
+});
+
+app.post("/api/signup", (req, res) => {
+  res.send();
 });
 
 app.listen(PORT, () => {
